d07608df95ddd6fd0cee6fa297461d0c3a0728ae
Spikethea <spike00890@hotmail.co.uk>
2023-12-22 09:42:14 +0000

    article sticky fix

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,15 +1,15 @@
 import Project from "./project";
-// import projectData from "../../data/projects.json";
+import projectData from "../../data/projects.json";
 import { projectProps } from "@components/ProjectEntry";
 
-// export async function generateStaticParams() {
-//     //const projects = await fetch('https://.../projects').then((res) => res.json())
-//     const projects = projectData.projects;
+export async function generateStaticParams() {
+    //const projects = await fetch('https://.../projects').then((res) => res.json())
+    const projects = projectData.projects;
 
-//     return projects.map((p) => ({
-//       project: p.name,
-//     }))
-// } 
+    return projects.map((p) => ({
+      project: p.name,
+    }))
+} 
 
 export default function Page({ params }: { params: projectProps}) {
     return <Project />
